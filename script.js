78d98c226f7f7186f21a578987e8a08e88904220
KavyaGupta15 <83356471+KavyaGupta15@users.noreply.github.com>
2021-06-04 19:30:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var names=new Array();
+names[0]=" Yakkov";
+names[1]=" jain";
+names[2]=" Parul";
+names[3]=" Jim";
+names[4]=" Farnk";
+names[5]=" Bet";
+names[6]=" Ben";
+names[7]=" Larry";
+names[8]=" Paul";
+names[9]=" Jason";
+for(var i=0; i< names.length; i++){
+    if(names[i].charAt(0)=="J" || names[i].charAt(0)=='j'){
+    console.log("Goodbye"+ names[i]);
+}
+else{
+    console.log("Hello"+ names[i])
+}
+}
+
+
+
+
+
